6068ec21b2e7e82e9bd8635e81ea8287aec42798
fedem96 <federicomagno96@gmail.com>
2020-02-13 10:53:30 +0100

    refactoring in Query

--- src/query/Query.java
+++ src/query/Query.java
@@ -8,6 +8,7 @@ import org.apache.hadoop.io.Text;
 import utils.Globals;
 
 import java.io.IOException;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -28,46 +29,63 @@ public class Query {
 
 
         SimpleDateFormat dateFormat = new SimpleDateFormat(Globals.datePattern, Locale.ENGLISH);
-        SequenceFile.Reader reader = new SequenceFile.Reader(conf, SequenceFile.Reader.file(new Path(Globals.batchOutputFile)));
-        Text key = new Text();
-        Text val = new Text();
 
         if(args.length == 1){ // specific date
             Date queryDate = dateFormat.parse(args[0]);
 
-            while (reader.next(key, val)) {
-                Date tweetDate = dateFormat.parse(key.toString());
-                if(tweetDate.compareTo(queryDate) == 0){
-                    String counts[] = val.toString().split(",");
-                    String numGood = counts[0];
-                    String numBad = counts[1];
-                    System.out.println("Num good tweets on " + dateFormat.format(queryDate) + ": " + numGood);
-                    System.out.println("Num bad tweets on " + dateFormat.format(queryDate) + ": " + numBad);
-                    break;
-                }
-            }
+
+            int nums[] = queryBatch(queryDate, queryDate, dateFormat, conf);
+            int numGood = nums[0];
+            int numBad = nums[1];
+
+//            nums = querySpeed();
+//            numGood += nums[0];
+//            numBad += nums[1];
+
+            System.out.println("Num good tweets on " + dateFormat.format(queryDate) + ": " + numGood);
+            System.out.println("Num bad tweets on " + dateFormat.format(queryDate) + ": " + numBad);
 
 
         } else { // dates interval
             Date beginDate = dateFormat.parse(args[0]);
             Date endDate = dateFormat.parse(args[1]);
 
-            int numGood = 0;
-            int numBad = 0;
-            while (reader.next(key, val)) {
-                Date tweetDate = dateFormat.parse(key.toString());
-                if(!beginDate.after(tweetDate) && !endDate.before(tweetDate)){
-                    String counts[] = val.toString().split(",");
-                    numGood += Integer.parseInt(counts[0]);
-                    numBad += Integer.parseInt(counts[1]);
-                }
-            }
+            int nums[] = queryBatch(beginDate, endDate, dateFormat, conf);
+            int numGood = nums[0];
+            int numBad = nums[1];
+
+//            nums = querySpeed();
+//            numGood += nums[0];
+//            numBad += nums[1];
 
             System.out.println("Num good tweets between " + dateFormat.format(beginDate) + " and " + dateFormat.format(endDate)  + " (included): " + numGood);
             System.out.println("Num bad tweets between " + dateFormat.format(beginDate) + " and " + dateFormat.format(endDate)  + " (included): " + numBad);
         }
 
-        reader.close();
     }
+
+    private static int[] queryBatch(Date beginDate, Date endDate, DateFormat dateFormat, Configuration conf) throws IOException {
+        SequenceFile.Reader reader = new SequenceFile.Reader(conf, SequenceFile.Reader.file(new Path(Globals.batchOutputFile)));
+        Text key = new Text();
+        Text val = new Text();
+        int[] nums = new int[2];
+        while (reader.next(key, val)) {
+            try {
+                Date tweetDate = dateFormat.parse(key.toString());
+                if((!beginDate.after(tweetDate) && !endDate.before(tweetDate))){
+                    String counts[] = val.toString().split(",");
+                    nums[0] += Integer.parseInt(counts[0]);
+                    nums[1] += Integer.parseInt(counts[1]);
+                    break;
+                }
+            } catch (ParseException e) {
+                System.err.println("Error: cannot parse date");
+            }
+        }
+        reader.close();
+        return nums;
+    }
+
+
 
 }
