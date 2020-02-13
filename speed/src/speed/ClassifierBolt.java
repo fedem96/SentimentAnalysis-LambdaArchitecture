bfdb65f486e07696a65ecca65a4372c2f8c309e3
iacopoerpichini <iacopo.erpichini@hotmail.it>
2020-02-13 08:37:29 +0100

    fix timestamp in speed layer

--- speed/src/speed/ClassifierBolt.java
+++ speed/src/speed/ClassifierBolt.java
@@ -8,8 +8,12 @@ import org.apache.storm.topology.base.BaseBasicBolt;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
+import utils.Globals;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 import java.util.Map;
 
 //TODO bolt
@@ -26,32 +30,42 @@ class ClassifierBolt extends BaseBasicBolt {
     }
 
     public void execute(Tuple tuple, BasicOutputCollector collector) {
-            // System.out.println(tuple);
-            values = tuple.getString(0).split(",",2);
-            //select the timestamp without time
-            String timestamp = values[0].substring(0,12) + values[0].substring(25,30);
-            String tweet = values[1];
+        // System.out.println(tuple);
+        values = tuple.getString(0).split(",",2);
+        //select the timestamp without time
+        String timestamp = values[0].substring(4,10) + values[0].substring(23,28);
+        // change timestamp format
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd yyyy", Locale.ENGLISH);
+        SimpleDateFormat newDateFormat = new SimpleDateFormat(Globals.datePattern, Locale.ENGLISH);
 
-            try {
-                //FIXME set path with args?!
-                classifier = new Classifier("/home/iacopo/Scrivania/Sentiment/dataset/classifier_weights.lpc");
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        try {
+            timestamp = newDateFormat.format(dateFormat.parse(timestamp));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
-            int sentiment;
-            // System.out.println(classifier.evaluateTweet(tweet));
-            if(classifier.evaluateTweet(tweet).equals("neg")){
-                sentiment = 0;
-            }else{
-                sentiment = 4;
-            }
+        String tweet = values[1];
 
-            collector.emit( new Values(timestamp, sentiment));
+        try {
+            //FIXME set path with args?!
+            classifier = new Classifier("/home/iacopo/Scrivania/Sentiment/dataset/classifier_weights.lpc");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-            System.out.println("BOLT CLASSIFIER: "+ timestamp + ", sentiment: " + sentiment);
+        int sentiment;
+        // System.out.println(classifier.evaluateTweet(tweet));
+        if(classifier.evaluateTweet(tweet).equals("neg")){
+            sentiment = 0;
+        }else{
+            sentiment = 4;
+        }
+
+        collector.emit( new Values(timestamp, sentiment));
+
+        System.out.println("BOLT CLASSIFIER: "+ timestamp + ", sentiment: " + sentiment);
     }
 
 
